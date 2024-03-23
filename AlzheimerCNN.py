963eee56b61e46a91cf594d8d9cd4909bc685571
Rudis <rudisarg@gmail.com>
2024-03-23 07:55:47 -0400

    added file paths and dataloaders

--- AlzheimerCNN.py
+++ AlzheimerCNN.py
@@ -4,17 +4,21 @@ import torch.optim as optim;
 import torchvision;
 import torchvision.transforms as transforms;
 from torchvision.datasets import ImageFolder;
-from torch.utils.data import dataloader;
+from torch.utils.data import DataLoader;
 import numpy as np;
 from sklearn.metrics import confusion_matrix;
 import matplotlib.pyplot as plt;
 import cv2;
+import os;
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # paths for the data
-trainingDataset = ImageFolder(root='/Users/rudisargueta/Documents/Alzheimer_CNN/AlzheimersDataset/train')
-testDataset = ImageFolder(root='/Users/rudisargueta/Documents/Alzheimer_CNN/AlzheimersDataset/test')
+datasetPath = '/Users/rudisargueta/Documents/Alzheimer_CNN/AlzheimersDataset'
+
+trainingSet = os.path.join(datasetPath, 'test')
+testSet = os.path.join(datasetPath, 'test')
+
 
 # preprocessing mri images from kaggale
 transform = transforms.Compose([
@@ -22,6 +26,15 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5, 0.5, 0.5])
 ])
+
+training = ImageFolder(trainingSet , transform=transform)
+test = ImageFolder(testSet, transform=transform)
+
+# dataloaders
+trainingLoader = DataLoader(training, batch_size=64, shuffle=True)
+testLoader = DataLoader(test, batch_size=64, shuffle=False)
+
+#
 
 
 
