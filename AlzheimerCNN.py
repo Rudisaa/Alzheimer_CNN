868af8a1a02c84453f3cb0b09bd8c9681fd04900
Rudis <rudisarg@gmail.com>
2024-03-23 11:47:23 -0400

    fixed call to constructor

--- AlzheimerCNN.py
+++ AlzheimerCNN.py
@@ -50,7 +50,7 @@ print(output.shape)
 
 class CNN(nn.Module):
     def __init__(self):
-        super(CNN.self).__init__() 
+        super(CNN, self).__init__() 
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
