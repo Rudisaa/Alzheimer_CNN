c889c95a0f763cb8a337653c46ab12fbe0a237c8
Rudis <rudisarg@gmail.com>
2024-03-23 11:33:26 -0400

    created first portion of cnn

--- AlzheimerCNN.py
+++ AlzheimerCNN.py
@@ -20,7 +20,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # paths for the data
 datasetPath = os.getenv('DATASET')
 
-trainingSet = os.path.join(datasetPath, 'test')
+trainingSet = os.path.join(datasetPath, 'train')
 testSet = os.path.join(datasetPath, 'test')
 
 
@@ -39,9 +39,24 @@ test = ImageFolder(testSet, transform=transform)
 trainingLoader = DataLoader(training, batch_size=64, shuffle=True)
 testLoader = DataLoader(test, batch_size=64, shuffle=False)
 
+m = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+m = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+#m = nn.Conv2d(32, 128, kernel_size=3, padding=1)
+m = nn.MaxPool2d(2,2)
+input = torch.randn(1,3,64,64)
+output = m(input)
+print(output.shape)
 # Creating a CNN 
 
-
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__(CNN, self.__init__()) 
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2,2)
+        self.fc1 = nn.Linear(8192, 128)
+        self.fc2 = nn.Linear(128, 2)
 
 
 
