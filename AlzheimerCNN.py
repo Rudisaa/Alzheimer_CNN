5ef41f838587c3679dbd0a2c8c4200aac53bd1fe
Rudis <rudisarg@gmail.com>
2024-03-23 07:41:49 -0400

    added paths and transform

--- AlzheimerCNN.py
+++ AlzheimerCNN.py
@@ -10,9 +10,20 @@ from sklearn.metrics import confusion_matrix;
 import matplotlib.pyplot as plt;
 import cv2;
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# paths for the data
+trainingDataset = ImageFolder(root='/Users/rudisargueta/Documents/Alzheimer_CNN/AlzheimersDataset/train')
+testDataset = ImageFolder(root='/Users/rudisargueta/Documents/Alzheimer_CNN/AlzheimersDataset/test')
 
 # preprocessing mri images from kaggale
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5, 0.5, 0.5])
+])
+
+
 
 
 
