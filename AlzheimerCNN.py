ed1800ee961eefe937211dc9bb63fa3315bd047e
Rudis <rudisarg@gmail.com>
2024-03-23 11:46:55 -0400

    fixed call

--- AlzheimerCNN.py
+++ AlzheimerCNN.py
@@ -50,14 +50,28 @@ print(output.shape)
 
 class CNN(nn.Module):
     def __init__(self):
-        super().__init__(CNN, self.__init__()) 
+        super(CNN.self).__init__() 
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.pool = nn.MaxPool2d(2,2)
         self.fc1 = nn.Linear(8192, 128)
         self.fc2 = nn.Linear(128, 2)
+    
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = self.pool(torch.relu(self.conv3(x)))
+        print(x.shape)
+        x = x.view(-1,8192)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+    
+
+model = CNN()
 
 
+input = torch.randn(1,3,128,128)
 
-
+output = model(input)
